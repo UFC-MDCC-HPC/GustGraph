f2902add029873ae995d46214a85c7dc8dc036cc
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-08-15 19:11:05 -0300

    Config

--- config-components.sh
+++ config-components.sh
@@ -2,3 +2,4 @@
 ./HPC-Shelf-MapReduce/scripts/create.project--br.________.sh
 ./HPC-Shelf-MapReduce/scripts/delete.bin.obj.MyClass--br.________.sh
 ./HPC-Shelf-MapReduce/scripts/sn--br.ufc.mdcc.hpcshelf.mapreduce._.Interface.sh
+
