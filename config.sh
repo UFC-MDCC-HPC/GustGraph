ec33479f82b6a33102d12084557c5d99a88d999d
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-08-15 18:20:43 -0300

    Config

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./scripts/create.project--br.________.sh
-./scripts/delete.bin.obj.MyClass--br.ufc.mdcc.________.sh
-./scripts/sn--br.ufc.mdcc.hpcshelf.mapreduce._.Interface.sh
+sh HPC-Shelf-MapReduce/scripts/create.project--br.________.sh
+sh HPC-Shelf-MapReduce/scripts/delete.bin.obj.MyClass--br.ufc.mdcc.________.sh
+sh HPC-Shelf-MapReduce/scripts/sn--br.ufc.mdcc.hpcshelf.mapreduce._.Interface.sh
