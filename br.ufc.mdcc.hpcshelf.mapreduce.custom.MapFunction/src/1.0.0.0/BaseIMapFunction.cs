badaa2cec6c64a5d85bc6f0a4216cb290ceef605
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-08-19 18:07:12 -0300

    Config

--- br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction/src/1.0.0.0/BaseIMapFunction.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction/src/1.0.0.0/BaseIMapFunction.cs
@@ -7,4 +7,15 @@ namespace br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction
 	public interface BaseIMapFunction : IComputationKind 
 	{
 	}
+	
+public interface BaseIMapFunction<IMK, IMV, OMK, OMV> : IComputationKind 
+		where IMK:IData
+		where IMV:IData
+		where OMK:IData
+		where OMV:IData
+{
+	IMK Input_key {get;}
+	IMV Input_value {get;}
+}
+
 }
--- br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction/src/1.0.0.0/IMapFunction.cs
+++ br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction/src/1.0.0.0/IMapFunction.cs
@@ -2,7 +2,9 @@ using br.ufc.pargo.hpe.kinds;
 
 namespace br.ufc.mdcc.hpcshelf.mapreduce.custom.MapFunction
 {
-	public interface IMapFunction : BaseIMapFunction
-	{
-	}
+	public interface IMapFunction<IMK, IMV, OMK, OMV> : BaseIMapFunction<IMK, IMV, OMK, OMV>
+		where IMK:IData
+		where IMV:IData
+		where OMK:IData
+		where OMV:IData
 }
