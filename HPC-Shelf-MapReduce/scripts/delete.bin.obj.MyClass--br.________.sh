61f605b625ccc3053533297cc198333f0521d386
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-08-30 01:10:50 -0300

    Merging Projects

--- HPC-Shelf-MapReduce/scripts/delete.bin.obj.MyClass--br.________.sh
+++ HPC-Shelf-MapReduce/scripts/delete.bin.obj.MyClass--br.________.sh
@@ -9,3 +9,14 @@ do
         echo rm -rf $pacote/obj
         rm -rf $pacote/obj
 done;
+
+for folder in teste.*
+do 
+        pacote=$folder/$folder
+        echo rm -rf $pacote/MyClass.cs
+        rm -rf $pacote/MyClass.cs
+        echo rm -rf $pacote/bin
+        rm -rf $pacote/bin
+        echo rm -rf $pacote/obj
+        rm -rf $pacote/obj
+done;
