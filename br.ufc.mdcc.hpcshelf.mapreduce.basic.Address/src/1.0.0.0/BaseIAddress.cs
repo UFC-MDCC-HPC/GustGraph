bbd62b842c3cf480b682be44d1f1b59c79a551e7
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-09-04 00:31:48 -0300

    Update

---
+++ br.ufc.mdcc.hpcshelf.mapreduce.basic.Address/src/1.0.0.0/BaseIAddress.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace br.ufc.mdcc.hpcshelf.mapreduce.basic.Address {
+	public class BaseIAddress {
+		public BaseIAddress() {
+		}
+	}
+}
+
---
+++ br.ufc.mdcc.hpcshelf.mapreduce.basic.Address/src/1.0.0.0/IAddress.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace br.ufc.mdcc.hpcshelf.mapreduce.basic.Address {
+	public class IAddress {
+		public IAddress() {
+		}
+	}
+}
+
