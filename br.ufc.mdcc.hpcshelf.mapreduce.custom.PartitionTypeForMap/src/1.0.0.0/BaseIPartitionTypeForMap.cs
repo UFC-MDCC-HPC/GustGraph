7ddcd23899880a9d66b831cdbca070c77abb0bec
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-09-09 02:09:40 -0300

    Update

---
+++ br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionTypeForMap/src/1.0.0.0/BaseIPartitionTypeForMap.cs
@@ -0,0 +1,11 @@
+/* AUTOMATICALLY GENERATE CODE */
+
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionType;
+
+namespace br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionTypeForMap
+{
+	public interface BaseIPartitionTypeForMap : BaseIPartitionType, IQualifierKind 
+	{
+	}
+}
---
+++ br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionTypeForMap/src/1.0.0.0/IPartitionTypeForMap.cs
@@ -0,0 +1,9 @@
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionType;
+
+namespace br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionTypeForMap
+{
+	public interface IPartitionTypeForMap : BaseIPartitionTypeForMap, IPartitionType
+	{
+	}
+}
