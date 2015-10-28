8a39c7e1bf495af3493e8580556d3e28301c1949
heron-carvalho <carvalho.heron@gmail.com>
2015-10-28 08:18:27 -0300

    =deleting some useless files

--- br.ufc.mdcc.hpcshelf.mapreduce.task.TaskPortTypeShuffleMap/src/1.0.0.0/ITaskPorttypeShuffle.cs
+++
@@ -1,9 +0,0 @@
-using br.ufc.pargo.hpe.kinds;
-using br.ufc.mdcc.hpc.storm.binding.task.TaskPortType;
-
-namespace br.ufc.mdcc.hpcshelf.mapreduce.task.TaskPortTypeShuffle
-{
-	public interface ITaskPorttypeShuffle : BaseITaskPorttypeShuffle, ITaskPortType
-	{
-	}
-}
