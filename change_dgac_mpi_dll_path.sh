df3fb7da67dcd43a2cdb251b1737b8ef22031656
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-04-20 16:38:27 -0300

    new path DGAC.dll and MPI.dll

--- change_dgac_mpi_dll_path.sh
+++ change_dgac_mpi_dll_path.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #DGAC.dll
-CURRENTPATH='opt\\mono-4.2.2\\lib\\mono\\DGAC\\DGAC.dll'
-NEWPATH='opt\\mono-4.2.2\\lib\\mono\\DGAC\\DGAC.dll'
+CURRENTPATH='..\\..\\lib\\DGAC.dll'
+NEWPATH='..\\..\\lib\\DGAC.dll'
 #NEWPATH='..\\opt\\mono-4.2.2\\lib\\mono\\DGAC\\DGAC.dll'
 find . -iname *.csproj | xargs sed -i 's/'$CURRENTPATH'/'$NEWPATH'/g'
 
 #MPI.dll
-CURRENTPATH='opt\\mono-4.2.2\\lib\\mono\\MPI\\MPI.dll'
-NEWPATH='opt\\mono-4.2.2\\lib\\mono\\MPI\\MPI.dll'
+CURRENTPATH='..\\..\\lib\\MPI.dll'
+NEWPATH='..\\..\\lib\\MPI.dll'
 #NEWPATH='..\\opt\\mono-4.2.2\\lib\\mono\\MPI\\MPI.dll'
 find . -iname *.csproj | xargs sed -i 's/'$CURRENTPATH'/'$NEWPATH'/g'
 
