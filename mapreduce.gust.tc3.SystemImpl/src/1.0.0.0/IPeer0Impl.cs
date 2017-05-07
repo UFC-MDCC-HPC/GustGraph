c9e238e7412743e862822fbcf8112a45292cd641
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-05-07 03:12:13 -0300

    Extends for Gust

--- mapreduce.gust.tc3.SystemImpl/src/1.0.0.0/IPeer0Impl.cs
+++ mapreduce.gust.tc3.SystemImpl/src/1.0.0.0/IPeer0Impl.cs
@@ -3,12 +3,9 @@ using br.ufc.pargo.hpe.backend.DGAC;
 using br.ufc.pargo.hpe.basic;
 using br.ufc.pargo.hpe.kinds;
 using mapreduce.gust.tc3.System;
-using br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSource;
 using System.Threading;
 using br.ufc.mdcc.hpcshelf.mapreduce.connector.Splitter;
 using br.ufc.mdcc.hpcshelf.platform.maintainer.DataHost;
-using br.ufc.mdcc.common.String;
-using br.ufc.mdcc.common.Integer;
 using br.ufc.mdcc.hpcshelf.mapreduce.custom.PartitionFunction;
 
 namespace mapreduce.gust.tc3.SystemImpl
@@ -21,18 +18,18 @@ namespace mapreduce.gust.tc3.SystemImpl
 
 		public override void main()
 		{
-			IDataSource<IDataHost> source = ((IDataSource<IDataHost>)(this.Services.getPort("source")));
-			Thread go_source = new Thread(new ParameterizedThreadStart(this.Go));
-            go_source.Start(source);
-            ISplitterCollector<IInteger, IString, IPartitionFunction<IInteger>> splitter_input = ((ISplitterCollector<IInteger, IString, IPartitionFunction<IInteger>>)(this.Services.getPort("splitter_input")));
-			Thread go_splitter_input = new Thread(new ParameterizedThreadStart(this.Go));
-			go_splitter_input.Start(splitter_input);
-            go_source.Join();
-            go_splitter_input.Join();
-
-			Console.WriteLine ("FINISH PEER 0");
-			AutoResetEvent e = new AutoResetEvent (false);
-			e.WaitOne ();
+//			IDataSource<IDataHost> source = ((IDataSource<IDataHost>)(this.Services.getPort("source")));
+//			Thread go_source = new Thread(new ParameterizedThreadStart(this.Go));
+//            go_source.Start(source);
+//            ISplitterCollector<IInteger, IString, IPartitionFunction<IInteger>> splitter_input = ((ISplitterCollector<IInteger, IString, IPartitionFunction<IInteger>>)(this.Services.getPort("splitter_input")));
+//			Thread go_splitter_input = new Thread(new ParameterizedThreadStart(this.Go));
+//			go_splitter_input.Start(splitter_input);
+//            go_source.Join();
+//            go_splitter_input.Join();
+//
+//			Console.WriteLine ("FINISH PEER 0");
+//			AutoResetEvent e = new AutoResetEvent (false);
+//			e.WaitOne ();
 		}
 	}
 }
--- mapreduce.gust.tc3.SystemImpl/src/1.0.0.0/IPeer7Impl.cs
+++ mapreduce.gust.tc3.SystemImpl/src/1.0.0.0/IPeer7Impl.cs
@@ -3,12 +3,9 @@ using br.ufc.pargo.hpe.backend.DGAC;
 using br.ufc.pargo.hpe.basic;
 using br.ufc.pargo.hpe.kinds;
 using mapreduce.gust.tc3.System;
-using br.ufc.mdcc.hpcshelf.mapreduce.datasource.DataSink;
 using br.ufc.mdcc.hpcshelf.mapreduce.connector.Splitter;
 using System.Threading;
 using br.ufc.mdcc.hpcshelf.platform.maintainer.DataHost;
-using br.ufc.mdcc.common.String;
-using br.ufc.mdcc.common.Integer;
 
 namespace mapreduce.gust.tc3.SystemImpl
 {
@@ -20,18 +17,18 @@ namespace mapreduce.gust.tc3.SystemImpl
 
 		public override void main()
 		{
-			IDataSink<IDataHost> sink = ((IDataSink<IDataHost>)(this.Services.getPort("sink")));
-			Thread go_sink = new Thread(new ParameterizedThreadStart(this.Go));
-			go_sink.Start(sink);
-			ISplitterFeeder<IString, IInteger> splitter_output = ((ISplitterFeeder<IString, IInteger>)(this.Services.getPort("splitter_output")));
-			Thread go_splitter_output = new Thread(new ParameterizedThreadStart(this.Go));
-			go_splitter_output.Start(splitter_output);
-			go_sink.Join();
-			go_splitter_output.Join();
-
-			Console.WriteLine ("FINISH PEER 3");
-			AutoResetEvent e = new AutoResetEvent (false);
-			e.WaitOne ();
+//			IDataSink<IDataHost> sink = ((IDataSink<IDataHost>)(this.Services.getPort("sink")));
+//			Thread go_sink = new Thread(new ParameterizedThreadStart(this.Go));
+//			go_sink.Start(sink);
+//			ISplitterFeeder<IString, IInteger> splitter_output = ((ISplitterFeeder<IString, IInteger>)(this.Services.getPort("splitter_output")));
+//			Thread go_splitter_output = new Thread(new ParameterizedThreadStart(this.Go));
+//			go_splitter_output.Start(splitter_output);
+//			go_sink.Join();
+//			go_splitter_output.Join();
+//
+//			Console.WriteLine ("FINISH PEER 3");
+//			AutoResetEvent e = new AutoResetEvent (false);
+//			e.WaitOne ();
 		}
 	}
 }
